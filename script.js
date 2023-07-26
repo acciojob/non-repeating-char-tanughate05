17878b6ee97498435ef43a4f873ad67de89dc022
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-07-26 19:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+//your JS code here. If requ
+let firstInput = prompt ("Enter string","aabbcdd");
+
+function firstNonReapeating(firstInput) {
+  for (var i = 0; i < firstInput.length; i++) {
+    var output = firstInput.charAt(i);
+    if (firstInput.indexOf(output) == i && firstInput.indexOf(output, i + 1) == -1) {
+      return output;
+    }
+  }
+  return null;
+}
+
+
+alert(firstNonReapeating(firstInput));
